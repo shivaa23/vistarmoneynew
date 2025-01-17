c0c1e4b4686c84d8566de3800a06812ac6fcf399
shivam <shivammca099gmail.com>
2025-01-17 13:47:57 +0530

    changes in pdf

--- src/component/RetailerPdf.jsx
+++ src/component/RetailerPdf.jsx
@@ -8,7 +8,7 @@ import {
   Image,
   Font,
 } from "@react-pdf/renderer";
-import { dilliPaysign, signAvatars } from "../iconsImports";
+import { dilliPaysign, Logo, signAvatars } from "../iconsImports";
 
 import robotoBold from "../fonts/Roboto-Bold.ttf";
 import { cBottom, cTop, impsguruStamp, loginPage1 } from "../iconsImports";
@@ -56,147 +56,156 @@ const styles = StyleSheet.create({
 const RetailerPdf = ({ user }) => {
   return (
     <Document>
-      <Page style={styles.page} orientation="landscape" size="B5">
-        <View style={{ postion: "relative" }}>
-          <Image
-            src={cTop}
-            style={{
-              position: "absolute",
-              top: "0px",
-              right: "-80px",
-              width: "550px",
-            }}
-          />
-          <Image
-            src={loginPage1}
-            style={{
-              width: "160px",
-              position: "absolute",
-              top: "25px",
-              left: "35px",
-            }}
-          />
-        </View>
-        <View
+    <Page style={styles.page} orientation="landscape" size="B5">
+      <View style={{ postion: "relative" }}>
+        <Image
+          src={cTop}
           style={{
-            position: "relative",
-            top: "120px",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            position: "absolute",
+            top: "0px",
+            right: "-80px",
+            width: "550px",
+          }}
+        />
+        <Image
+          src={Logo}
+          style={{
+            width: "160px",
+            position: "absolute",
+            top: "25px",
+            left: "35px",
+          }}
+        />
+      </View>
+      <View
+        style={{
+          position: "relative",
+          top: "120px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: "50px",
+            letterSpacing: "1px",
+            color: "#1D2B5D",
+            fontFamily: "roboto-bold",
           }}
         >
+          CERTIFICATE
+        </Text>
+        <Text
+          style={{
+            fontSize: "15px",
+            letterSpacing: "0.4px",
+            marginTop: "5px",
+            opacity: "0.7",
+          }}
+        >
+          Of Appointment
+        </Text>
+        <Text
+          style={{
+            fontSize: "30px",
+            letterSpacing: "0.4px",
+            marginVertical: "20px",
+            marginBottom: "8px",
+            fontFamily: "roboto-bold",
+          }}
+        >
+          {user?.name}
+        </Text>
+        <Text
+          style={{
+            fontSize: "12px",
+            letterSpacing: "0.4px",
+            marginVertical: "6px",
+            opacity: "0.7",
+          }}
+        >
+          is an Authorized{" "}
           <Text
             style={{
-              fontSize: "50px",
-              letterSpacing: "1px",
-              color: "#1D2B5D",
+              fontSize: "12px",
               fontFamily: "roboto-bold",
+              letterSpacing: "0.4px",
             }}
           >
-            CERTIFICATE
+            {user?.role === "Ad"
+              ? "Distributor"
+              : user?.role === "Ret"
+              ? "Retailer"
+              : user?.role === "Dd"
+              ? "Direct Dealer"
+              : ""}
           </Text>
+        </Text>
+        <Text
+          style={{
+            fontSize: "12px",
+            letterSpacing: "0.4px",
+            marginVertical: "6px",
+            opacity: "0.7",
+          }}
+        >
+          Mobifast Solutions Pvt Ltd
+        </Text>
+        <Text
+          style={{
+            fontSize: "12px",
+            letterSpacing: "0.4px",
+            marginVertical: "6px",
+            opacity: "0.7",
+          }}
+        >
+          for providing Banking{" "}
           <Text
             style={{
-              fontSize: "15px",
-              letterSpacing: "0.4px",
-              marginTop: "5px",
-              opacity: "0.7",
-            }}
-          >
-            Of Appointment
-          </Text>
-          <Text
-            style={{
-              fontSize: "30px",
-              letterSpacing: "0.4px",
-              marginVertical: "20px",
-              marginBottom: "8px",
+              fontSize: "12px",
               fontFamily: "roboto-bold",
+              letterSpacing: "0.4px",
             }}
           >
-            {user?.name}
-          </Text>
+            Business Correspondent services
+          </Text>{" "}
+          during{" "}
           <Text
             style={{
               fontSize: "12px",
+              fontFamily: "roboto-bold",
               letterSpacing: "0.4px",
-              marginVertical: "6px",
-              opacity: "0.7",
             }}
           >
-            is an Authorized{" "}
-            <Text
-              style={{
-                fontSize: "12px",
-                fontFamily: "roboto-bold",
-                letterSpacing: "0.4px",
-              }}
-            >
-              {user?.role === "Ad"
-                ? "Distributor"
-                : user?.role === "Ret"
-                ? "Retailer"
-                : user?.role === "Dd"
-                ? "Direct Dealer"
-                : ""}
-            </Text>
+            FY 2023- 2024
           </Text>
-          <Text
-            style={{
-              fontSize: "12px",
-              letterSpacing: "0.4px",
-              marginVertical: "6px",
-              opacity: "0.7",
-            }}
-          >
-            VistarMoney TECHNOLOGIES LIMITED 
-          </Text>
-          <Text
-            style={{
-              fontSize: "12px",
-              letterSpacing: "0.4px",
-              marginVertical: "6px",
-              opacity: "0.7",
-            }}
-          >
-            for providing Banking{" "}
-            <Text
-              style={{
-                fontSize: "12px",
-                fontFamily: "roboto-bold",
-                letterSpacing: "0.4px",
-              }}
-            >
-           Business Correspondent services
-       </Text>{" "}
-           
-          </Text>
-        </View>
-        <View style={{ postion: "relative" }}>
-          <Image
-            src={cBottom}
-            style={{
-              position: "absolute",
-              bottom: "-280px",
-              left: "-90px",
-              width: "550px",
-            }}
-          />
-        </View>
-        <View style={{ postion: "relative" }}>
-          <Image
-            src={signAvatars}
-            style={{
-              position: "absolute",
-              bottom: "-250px",
-              right: "50px",
-              width: "150px",
-            }}
-          />
-        </View>
-      </Page>
-    </Document>
+        </Text>
+      </View>
+      <View style={{ postion: "relative" }}>
+        <Image
+          src={cBottom}
+          style={{
+            position: "absolute",
+            bottom: "-280px",
+            left: "-90px",
+            width: "550px",
+          }}
+        />
+      </View>
+      <View style={{ postion: "relative" }}>
+        <Image
+          src={impsguruStamp}
+          style={{
+            position: "absolute",
+            bottom: "-250px",
+            right: "50px",
+            width: "200px",
+          }}
+        />
+      </View>
+    </Page>
+  </Document>
   );
 };
 
